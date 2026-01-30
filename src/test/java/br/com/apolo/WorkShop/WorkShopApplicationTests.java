68a62e4b3db67575d7362b90f39fa402a0e9912d
Thiago Murilo <thiagomayta10@gmail.com>
2026-01-30 10:20:51 -0300

    Projeto criado

---
+++ src/test/java/br/com/apolo/WorkShop/WorkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apolo.WorkShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
