847a8428246547de83627d21b8c23a80d74bd7ae
Thiago Murilo <thiagomayta10@gmail.com>
2026-02-04 16:24:43 -0300

    feat: adiciona camada de serviço e endpoints REST para User

--- src/main/java/br/com/apolo/WorkShop/service/UserService.java
+++ src/main/java/br/com/apolo/WorkShop/service/UserService.java
@@ -1,4 +1,25 @@
 package br.com.apolo.WorkShop.service;
 
+import br.com.apolo.WorkShop.entities.User;
+import br.com.apolo.WorkShop.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
 }
